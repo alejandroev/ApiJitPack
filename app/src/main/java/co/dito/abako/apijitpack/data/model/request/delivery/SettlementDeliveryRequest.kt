76a235779f7eaeaf240771ffcc57fedd679dfc1a
juantobon <juan.tobon@condorlabs.io>
2022-08-19 21:49:24 -0500

    Rename idSettlement to deliverySettlement in SettlementDeliveryRequest.kt

--- app/src/main/java/co/dito/abako/apijitpack/data/model/request/delivery/SettlementDeliveryRequest.kt
+++ app/src/main/java/co/dito/abako/apijitpack/data/model/request/delivery/SettlementDeliveryRequest.kt
@@ -3,7 +3,7 @@ package co.dito.abako.apijitpack.data.model.request.delivery
 import com.google.gson.annotations.SerializedName
 
 data class SettlementDeliveryRequest(
-    @SerializedName("Id") val idSettlement: Double,
-    @SerializedName("DllLiq") val settlementDetailDeliveryRequest: List<SettlementDetailDeliveryRequest>,
+    @SerializedName("Id") val deliveryId: Double,
+    @SerializedName("DllLiq") val settlementDetail: List<SettlementDetailDeliveryRequest>,
     @SerializedName("DllOc") val otherCharges: List<OtherChargesDetail>
 )
