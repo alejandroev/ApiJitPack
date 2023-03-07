edfd7dbe10f4399f050675a85137aedb0805a4ff
juantobonq <101196521+juantobonq@users.noreply.github.com>
2023-03-07 14:33:37 -0500

    Cancel order

--- app/src/main/java/co/dito/abako/apijitpack/domain/general/usecase/CancelDocumentUseCase.kt
+++ app/src/main/java/co/dito/abako/apijitpack/domain/general/usecase/CancelDocumentUseCase.kt
@@ -2,14 +2,14 @@ package co.dito.abako.apijitpack.domain.general.usecase
 
 import co.dito.abako.apijitpack.data.model.request.general.CancelDocumentRequest
 import co.dito.abako.apijitpack.data.model.response.general.MessageResponse
-import co.dito.abako.apijitpack.domain.general.GeneralRepository
+import co.dito.abako.apijitpack.domain.order.OrderRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
 class CancelDocumentUseCase @Inject constructor(
-    private val generalRepository: GeneralRepository
+    private val orderRepository: OrderRepository
 ) {
 
     suspend operator fun invoke(cancelDocumentRequest: CancelDocumentRequest): Flow<MessageResponse> =
-        generalRepository.cancelDocumentResponse(cancelDocumentRequest)
+        orderRepository.cancelDocumentResponse(cancelDocumentRequest)
 }
