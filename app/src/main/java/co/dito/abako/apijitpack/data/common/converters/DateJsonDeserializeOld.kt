d7a719753dafde147886c6e35404d484c7f44dd2
Juan Marin <juanmarin127590@gmail.com>
2024-06-03 09:42:13 -0500

    version  193

--- app/src/main/java/co/dito/abako/apijitpack/data/common/converters/DateJsonDeserializeOld.kt
+++ app/src/main/java/co/dito/abako/apijitpack/data/common/converters/DateJsonDeserializeOld.kt
@@ -4,6 +4,7 @@ import co.dito.abako.apijitpack.data.common.utils.convertirFechaLong
 import co.dito.abako.apijitpack.data.common.utils.stringToTimeOld
 import com.google.gson.*
 import java.lang.reflect.Type
+import java.text.SimpleDateFormat
 import java.util.*
 
 class DateJsonDeserializeOld : JsonDeserializer<Date> {
@@ -22,4 +23,18 @@ class DateJsonDeserializeOldApi : JsonDeserializer<Date> {
         typeOfT: Type?,
         context: JsonDeserializationContext?
     ): Date = Date((if(!json?.asString.isNullOrEmpty()) json?.asString?.convertirFechaLong() else 0)!!)
+}
+
+class DateJsonDeserialize : JsonDeserializer<Date> {
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): Date {
+        return json?.asString?.let {
+            dateFormat.parse(it)
+        } ?: Date(0) // Devuelve la fecha por defecto en caso de cadena nula o vacía
+    }
 }
--- app/src/main/java/co/dito/abako/apijitpack/data/model/response/delivery/DeliverySyncResponse.kt
+++ app/src/main/java/co/dito/abako/apijitpack/data/model/response/delivery/DeliverySyncResponse.kt
@@ -1,5 +1,6 @@
 package co.dito.abako.apijitpack.data.model.response.delivery
 
+import co.dito.abako.apijitpack.data.common.converters.DateJsonDeserialize
 import co.dito.abako.apijitpack.data.common.converters.DateJsonDeserializeOld
 import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
@@ -27,8 +28,8 @@ data class DeliverySyncResponse(
     @SerializedName("id") val id: Int,
     @SerializedName("doc") val doc: String,
     @SerializedName("obs") val obs: String,
-    @JsonAdapter(DateJsonDeserializeOld::class) @SerializedName("fcEnt") val fcEnt: Date,
-    @JsonAdapter(DateJsonDeserializeOld::class) @SerializedName("fcCre") val fcCre: Date,
+    @JsonAdapter(DateJsonDeserialize::class) @SerializedName("fcEnt") val fcEnt: Date,
+    @JsonAdapter(DateJsonDeserialize::class) @SerializedName("fcCre") val fcCre: Date,
     @SerializedName("facts") val facts: Int,
     @SerializedName("ctd") val ctd: Double,
     @SerializedName("crdt") val crdt: Double,
