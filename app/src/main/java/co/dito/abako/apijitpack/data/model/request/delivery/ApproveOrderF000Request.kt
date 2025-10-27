b86e17e71f6cc008b290bf1d8b21da7092c6dba7
Wilson Sierra <wilsonsierra@MacBook-Air-de-Wilson.local>
2025-10-27 17:15:44 -0500

    se ajusto el request con los campos que faltaban paraa probar un pedido

--- app/src/main/java/co/dito/abako/apijitpack/data/model/request/delivery/ApproveOrderF000Request.kt
+++ app/src/main/java/co/dito/abako/apijitpack/data/model/request/delivery/ApproveOrderF000Request.kt
@@ -2,5 +2,7 @@ package co.dito.abako.apijitpack.data.model.request.delivery
 
 data class ApproveOrderF000Request(
     val idPedido: Int,
-    val idUsuario: Int
+    val idUsuario: Int,
+    val detallePago: List<Any> = emptyList(),
+    val detalleOtrosConceptos: List<Any> = emptyList()
 )
